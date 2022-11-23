a1eb0eb792eedef15f318d2eb07d6dbd86134559
Alif Delgado <alifdelgado@gmail.com>
2022-11-23 16:51:23 -0600

    DBZ layout were added

---
+++ src/modules/dbz/layouts/DbzLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dbz-layout">
+    <h3>DBZ Layout</h3>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+.dbz-layout {
+  background: deepskyblue;
+}
+</style>
